bf2641e5795c4013c82d914de3c897856b6bc317
TristonJ <triston@rolltechbowling.com>
2016-12-31 15:40:41 -0800

    Finished the home page (kinda?)

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express'),
 
 var app = express();
 
+const CACHE_TIME = 100 * 60 * 60 * 24 * 7;
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', {maxAge: CACHE_TIME}));
 
 /**
  * Routers
