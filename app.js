862c82020bf70059e4c6eab568edbf135af9e2fa
TristonJ <triston@rolltechbowling.com>
2017-01-02 18:22:35 -0800

    Added Google analytics

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
   res.set('X-Powered-By', 'Triston Jones!');
   res.set('X-XSS-Protection', '1; mode=block');
   res.set('Content-Security-Policy',
-    "default-src 'none'; img-src 'self';" +
-    "script-src 'self' https://cdnjs.cloudflare.com;" +
+    "default-src 'none'; img-src 'self' https://www.google-analytics.com;" +
+    "script-src 'self' https://cdnjs.cloudflare.com https://www.google-analytics.com;" +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com;" +
     "font-src 'self' https://fonts.gstatic.com data:;");
   next();
